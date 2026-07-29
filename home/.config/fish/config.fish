f98d81926b571d54fd25a212feb9f805e97840fe
prezus <prezus@gmail.com>
2026-07-29 08:38:36 -0700

    chore: absorb OrbStack fish init + Pi settings bump
    Both files are stowed, so the tools wrote into the repo directly. OrbStack
    re-adds its config.fish line if removed; Pi bumped lastChangelogVersion and
    added quietStartup.

--- home/.config/fish/config.fish
+++ home/.config/fish/config.fish
@@ -7,3 +7,7 @@
 
 # Disable the interactive greeting
 set -g fish_greeting ""
+
+# Added by OrbStack: command-line tools and integration
+# This won't be added again if you remove it.
+source ~/.orbstack/shell/init2.fish 2>/dev/null || :
